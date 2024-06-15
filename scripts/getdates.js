94dbac588ee7861d0f7ec8f1a47d7836df8b0527
Kehinde Odumosu <odu23006@byui.edu>
2024-06-15 19:42:32 +0100

    My week1

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+const currentyear = document.querySelector("#currentyear");
+
+
+// use the date object
+const today = new Date();
+
+currentyear.innerHTML = `&copy;${today.getFullYear()} 🐱‍👤 Odumosu Kehinde Olusegun 🐱‍👤 Nigeria`;
+
+const lastModified = document.querySelector("#lastModified")
+
+lastModified.innerHTML = `Last Modification ${document.lastModified}`;
